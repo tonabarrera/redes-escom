4d9bdcd23e59ea024eb38bf7cbdadda23804551f
Carlos Barrera <carlostonatihu@gmail.com>
2017-04-15 00:39:06 -0500

    Nada

---
+++ Practica4/src/practica4/Envia.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica4;
+
+/**
+ *
+ * @author tona
+ */
+public class Envia {
+    public static void main (String[] args) {
+        System.out.println("Hola");
+    }
+}
