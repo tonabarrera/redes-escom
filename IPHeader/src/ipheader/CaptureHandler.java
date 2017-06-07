aa71a5f7a9c14b68b8f64644841a0af8b0ea5d52
Carlos Barrera <carlostonatihu@gmail.com>
2017-06-07 12:01:06 -0500

    Ya esta ip o eso parece

--- IPHeader/src/ipheader/CaptureHandler.java
+++ IPHeader/src/ipheader/CaptureHandler.java
@@ -20,30 +20,37 @@ public class CaptureHandler implements PcapPacketHandler<String> {
                 packet.getCaptureHeader().wirelen(), // Original length
                 user);                                 // User supplied object
 
-        System.out.println("\nEncabezado:\n" + packet.toHexdump());
         Ip4 ip4 = new Ip4();
         if (packet.hasHeader(ip4)) {
+            System.out.println("\nEncabezado:\n" + packet.toHexdump());
             System.out.println("Datos del encabezado IPv4");
-            System.out.println("Version: " + ip4.version());
-            System.out.println("Header length: " +ip4.hlen());
-            System.out.println("Type of Service: " + ip4.tos());
+            System.out.printf("%s .... = Version: %d\n", Integer.toBinaryString(ip4.version()),
+                    ip4.version());
+            System.out.printf(".... %s Header length: %d bytes (%X)\n",
+                    Integer.toBinaryString(ip4.hlen()), ip4.hlen(), ip4.hlen());
+            System.out.printf("0x%02X = Type of service: %s\n", ip4.tos(), ip4.tos());
             System.out.println("Total length: " +ip4.length());
-            System.out.println("Identifier: " + ip4.id());
-            System.out.println("Flags: " +ip4.flags());
+            System.out.printf("Identifier: 0x%04X %s\n", ip4.id(), ip4.id());
+            System.out.printf("Flags: 0x%02X", ip4.flags());
             System.out.println("Fragment Offset: " + ip4.offset());
-            System.out.println("Time to live: " +ip4.ttl());
-           // System.out.println("Protocol: " + ip4.pro());
-            System.out.println("Header Checksum: " +ip4.checksum());
+            System.out.println("Time to live: " + ip4.ttl());
+            System.out.printf("Protocol:%s Protocol (%d)\n", ip4.typeDescription().split(":")[1],
+                    ip4.type
+                    ());
+            System.out.printf("Header Checksum: 0x%04X\n", ip4.checksum());
             System.out.println("Source Address: ");
-            for (byte valor : ip4.source())
-                System.out.printf("%02X ", valor);
+            for (int i=0; i<ip4.source().length; i++) {
+                int dato = (ip4.source()[i]<0) ? ip4.source()[i]+256 : ip4.source()[i];
+                System.out.print(dato);
+                if (i!=ip4.source().length-1) System.out.print(".");
+            }
             System.out.println();
-            System.out.println("Destination Address: ");
-            for (byte valor : ip4.destination())
-                System.out.printf("%02X ", valor);
+            for (int i=0; i<ip4.destination().length; i++) {
+                int dato = (ip4.destination()[i]<0) ? ip4.destination()[i]+256 : ip4.destination()[i];
+                System.out.print(dato);
+                if (i!=ip4.destination().length-1) System.out.print(".");
+            }
             System.out.println();
-            //System.out.println("Options: " + ip4.());
-            //System.out.println("Padding: " +ip4());
         }
     }
 }
--- IPHeader/src/ipheader/IPHeader.java
+++ IPHeader/src/ipheader/IPHeader.java
@@ -22,61 +22,87 @@ import static org.jnetpcap.packet.format.FormatUtils.asString;
  * @author tona
  */
 public class IPHeader {
+    private static final String ARCHIVO = "samples.pcap";
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        List<PcapIf> alldevs = new ArrayList<>();
+        Pcap pcap = null;
+        CaptureHandler handler = new CaptureHandler();
         StringBuilder errbuf = new StringBuilder();
-        int r = Pcap.findAllDevs(alldevs, errbuf);
-        if (r == Pcap.NOT_OK || alldevs.isEmpty()) {
-            System.err.printf("Can't read list of devices, error is %s\n", errbuf.toString());
-            return; // Adios popo
-        }
-        System.out.println("Network devices found:");
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("---Elige un mode de captura--");
+        System.out.println("0.- Al vuelo");
+        System.out.println("1.- De un archivo guardado");
+        int opcion = 0;
         try {
-            int i = 0;
-            for (PcapIf device : alldevs) {
-                String description = (device.getDescription() != null) ?
-                        device.getDescription() : "No description available";
-                final byte[] mac = device.getHardwareAddress();
-                String dir_mac = (mac == null) ? "No tiene direccion MAC" : asString(mac);
-                System.out.printf("#%d: %s [%s] MAC:[%s]\n", i++, device.getName(),
-                                  description, dir_mac);
-            }// final - for
-
-            BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("---Elige la interfaz de captura---"); //Tarjeta de red
-            int nic = Integer.parseInt(buffer.readLine());
-            PcapIf device = alldevs.get(nic); // We know we have at least 1 device
-            System.out.printf("\nChoosing '%s' on your behalf:\n",
-                              (device.getDescription() != null) ? device.getDescription() : device.getName());
-            int snaplen = 64 * 1024;           // Capture all packets, no trucation
-            int flags = Pcap.MODE_PROMISCUOUS; // capture all packets
-            int timeout = 10 * 1000;           // 10 seconds in millis
-            Pcap pcap = Pcap.openLive(device.getName(), snaplen, flags, timeout, errbuf);
-
-            if (pcap == null) {
-                System.err.printf("Error while opening device for capture: " + errbuf.toString());
-                return;
-            }//if
-            /********F I L T R O********/
-            PcapBpfProgram filter = new PcapBpfProgram();
-            String expression = ""; // "port 80";
-            int optimize = 0; // 1 means true, 0 means false
-            int netmask = 0;
-            int r2 = pcap.compile(filter, expression, optimize, netmask);
-            if (r2 != Pcap.OK) //tiene que ser 0
-                System.out.println("Filter error: " + pcap.getErr());
-            //end if
-            pcap.setFilter(filter);
-            CaptureHandler handler = new CaptureHandler();
-            pcap.loop(5, handler, "IP HEADER");
-            pcap.close();
+            opcion = Integer.parseInt(buffer.readLine());
         } catch (IOException e) {
             e.printStackTrace();
         }
+        /*Al vuelo*/
+        if (opcion==0) {
+            List<PcapIf> alldevs = new ArrayList<>();
+            int r = Pcap.findAllDevs(alldevs, errbuf);
+            if (r == Pcap.NOT_OK || alldevs.isEmpty()) {
+                System.err.printf("Can't read list of devices, error is %s\n", errbuf.toString());
+                return; // Adios popo
+            }
+            System.out.println("Network devices found:");
+            try {
+                int i = 0;
+                for (PcapIf device : alldevs) {
+                    String description = (device.getDescription() != null) ? device.getDescription() :
+                            "No description available";
+                    final byte[] mac = device.getHardwareAddress();
+                    String dir_mac = (mac == null) ? "No tiene direccion MAC" : asString(mac);
+                    System.out.printf("#%d: %s [%s] MAC:[%s]\n", i++, device.getName(), description,
+                            dir_mac);
+                }// final - for
+
+
+                System.out.println("---Elige la interfaz de captura---"); //Tarjeta de red
+                int nic = Integer.parseInt(buffer.readLine());
+                PcapIf device = alldevs.get(nic); // We know we have at least 1 device
+                System.out.printf("\nChoosing '%s' on your behalf:\n",
+                        (device.getDescription() != null) ? device.getDescription() : device.getName());
+                int snaplen = 64 * 1024;           // Capture all packets, no trucation
+                int flags = Pcap.MODE_PROMISCUOUS; // capture all packets
+                int timeout = 10 * 1000;           // 10 seconds in millis
+                pcap = Pcap.openLive(device.getName(), snaplen, flags, timeout, errbuf);
+
+                if (pcap == null) {
+                    System.err.printf("Error while opening device for capture: " + errbuf.toString());
+                    return;
+                }//if
+
+            /*F I L T R O*/
+                PcapBpfProgram filter = new PcapBpfProgram();
+                String expression = ""; // "port 80";
+                int optimize = 0; // 1 means true, 0 means false
+                int netmask = 0;
+                int r2 = pcap.compile(filter, expression, optimize, netmask);
+                if (r2 != Pcap.OK) //tiene que ser 0
+                    System.out.println("Filter error: " + pcap.getErr());
+                //end if
+                pcap.setFilter(filter);
+
+                pcap.loop(50, handler, "IP HEADER");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            /*Abrir archivo*/
+            pcap = Pcap.openOffline(ARCHIVO, errbuf);
+            if (pcap == null) {
+                System.err.println("Error while opening device for capture: " + errbuf.toString());
+                return;
+            }
+            pcap.loop(5, handler, "REDES");
+        }
+        if (pcap != null)
+            pcap.close();
     }
     
 }
